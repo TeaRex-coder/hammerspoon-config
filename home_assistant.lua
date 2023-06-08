8df30678929034a21b783087129ca0c23e12b090
TeaRex-coder <83100473+TeaRex-coder@users.noreply.github.com>
2023-06-07 22:16:09 -0400

    add brightness controls

--- home_assistant.lua
+++ home_assistant.lua
@@ -13,6 +13,30 @@ end
 
 hs.hotkey.bind({"cmd", "ctrl"}, "1", toggleAlexBedroom)
 
+function dimAlexBedroom()
+  local url = env.HASS_URL .. "/api/services/light/turn_on"
+  local headers = {
+    ["Authorization"] = "Bearer " .. env.HASS_TOKEN,
+    ["Content-Type"] = "application/json"
+  }
+  local body = '{"entity_id": "light.alexs_bedroom", "brightness_step": -20}'
+  hs.http.post(url, body, headers)
+end
+
+hs.hotkey.bind({"cmd", "ctrl"}, "-", dimAlexBedroom)
+
+function brightenAlexBedroom()
+  local url = env.HASS_URL .. "/api/services/light/turn_on"
+  local headers = {
+    ["Authorization"] = "Bearer " .. env.HASS_TOKEN,
+    ["Content-Type"] = "application/json"
+  }
+  local body = '{"entity_id": "light.alexs_bedroom", "brightness_step": 20}'
+  hs.http.post(url, body, headers)
+end
+
+hs.hotkey.bind({"cmd", "ctrl"}, "=", brightenAlexBedroom)
+
 --toggle alex's desk lamp
 function toggleAlexDeskLamp()
     local url = env.HASS_URL .. "/api/services/light/toggle"
