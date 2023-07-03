84b4026ff3fcf54bedfd1fa6663bb6d01d4f05b6
TeaRex-coder <alexandre.ottawa@gmail.com>
2023-07-03 11:38:50 -0400

    remove slack window

--- miscellaneous.lua
+++ miscellaneous.lua
@@ -1,21 +1,9 @@
--- open new Slack window
-local function SlackWindow()
-    hs.execute("open -n /Applications/Slack.app")
-  end
-  
-hs.hotkey.bind({"ctrl", "shift"}, "n", function()
-    local slack = hs.application.get("Slack")
-    if slack ~= nil and slack:isFrontmost() then
-      SlackWindow()
-    end
-  end)
- 
 -- macbook display smaller
 hs.hotkey.bind(hyper, "[", function()
   hs.execute("/usr/local/bin/displayplacer 'id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:2048x1280 hz:60 color_depth:8 scaling:off origin:(-2048,0) degree:0'")
 end)
 
--- macbook display larger
+-- macbook display smaller
 hs.hotkey.bind(hyper, "]", function()
     hs.execute("/usr/local/bin/displayplacer 'id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1680x1050 hz:60 color_depth:8 scaling:on origin:(0,0) degree:0'")
 end)
